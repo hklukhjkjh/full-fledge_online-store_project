d134af06a5a9c5daa9e8e9e6e2a1ab2c0ab61bf3
hklukhjkjh <magfentesi@gmail.com>
2026-04-03 23:23:51 +0300

    add models and admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Products
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name"]
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "quantity", "price"]
+    search_fields = ["name", "description"]
+    list_filter = ["quantity", "category"]
+    fields = [
+        "name",
+        "category",
+        "slug",
+        "description",
+        "image",
+        "price",
+        "quantity",
+    ]
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,4 +1,62 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name="Название",
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        blank=True,
+        null=True,
+        verbose_name="URL",
+    )
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "Категорию"
+        verbose_name_plural = "Категории"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="products_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ("id",)
+
+    def __str__(self):
+        return f"{self.name} Количество - {self.quantity}"
+
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+
+    def display_id(self):
+        return f"{self.id:05}"
